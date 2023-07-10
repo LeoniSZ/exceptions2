34910ddf14a9bdafb47e0f2d83ab0f2359681dc5
Elioenai Roberto <elioenaipro@gmail.com>
2023-07-10 02:06:10 -0300

    bug fix

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,26 +7,26 @@ import model.exception.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
 		
-		System.out.print("Holder: ");
-		String holder = sc.nextLine();
-		
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
-		
-		
-		BankAccount bankAccount = new BankAccount(number, holder, balance, withdrawLimit);
 		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			BankAccount bankAccount = new BankAccount(number, holder, balance, withdrawLimit);
 			System.out.println("Enter amount for withdraw");
 			Double amount = sc.nextDouble();
 			bankAccount.withdraw(amount);
